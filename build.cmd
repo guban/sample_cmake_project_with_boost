fc93f760e70c06300207fadd34f627e4dd19dc57
Oleg Gubanov <oleg.gubanov@gmail.com>
2017-06-26 16:31:18 -0400

    Generate x64 build

--- build.cmd
+++ build.cmd
@@ -1,14 +1,11 @@
 @echo off
 set PATH="C:\Program Files\CMake\bin";%PATH%
 set build_dir=build
+set gen="Visual Studio 15 2017 Win64"
 
-if not exist %build_dir% (
-	mkdir %build_dir%
-	cd %build_dir%
-	cmake ..
-) else (
-	cd %build_dir%
-	cmake ..
-)
+if not exist %build_dir% mkdir %build_dir%
+cd %build_dir%
+
+cmake -G %gen% ..
 
 cmake --build .
