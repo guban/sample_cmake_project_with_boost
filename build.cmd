83cc1e8452cf3cb5a4a17de9376a18bc2a1061d6
Oleg Gubanov <oleg.gubanov@gmail.com>
2017-06-26 14:00:57 -0400

    Skip "cmake .." if the "build" subfolder already exists

--- build.cmd
+++ build.cmd
@@ -1,7 +1,13 @@
 @echo off
 set PATH="C:\Program Files\CMake\bin";%PATH%
 set build_dir=build
-if not exist %build_dir% mkdir %build_dir%
-cd %build_dir%
-cmake ..
+
+if not exist %build_dir% (
+	mkdir %build_dir%
+	cd %build_dir%
+	cmake ..
+) else (
+	cd %build_dir%
+)
+
 cmake --build .
