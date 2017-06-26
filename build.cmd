1aa206f25f8be33b7e96f67b8cf433966bcb1548
Oleg Gubanov <oleg.gubanov@gmail.com>
2017-06-26 16:43:10 -0400

    Use two-level "build" folder hierarchy

--- build.cmd
+++ build.cmd
@@ -1,11 +1,11 @@
 @echo off
 set PATH="C:\Program Files\CMake\bin";%PATH%
-set build_dir=build
 set gen="Visual Studio 15 2017 Win64"
+set build_dir="build\%gen%"
 
-if not exist %build_dir% mkdir %build_dir%
-cd %build_dir%
+if not exist "%build_dir%" mkdir "%build_dir%"
+cd "%build_dir%"
 
-cmake -G %gen% ..
+cmake -G %gen% ..\..
 
 cmake --build .
